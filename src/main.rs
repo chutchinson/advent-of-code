c5f1109000f81888b637dab262bdb3b8c7368e76
Chris Hutchinson <chris@cshutchinson.com>
2019-12-08 23:03:26 -0500

    day 4 solution

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ mod intcode;
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {
     let days: Vec<fn()> = vec![
         day1::solve,
         day2::solve,
-        day3::solve
+        day3::solve,
+        day4::solve
     ];
     for (index, solve) in days.iter().enumerate() {
         println!("day {}", index + 1);
